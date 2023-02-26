8f252192b15dfd830a45791467432a1509dbec3c
Jonathan Velez <jonathan14982@gmail.com>
2023-02-25 19:24:26 -0500

    working login api

---
+++ LAMPAPI/login.php
@@ -0,0 +1,68 @@
+<?php
+
+    $inData = getRequestInfo();
+
+    //Get fields from clients JSON POST
+	$userid = 0;
+	$firstName = "";
+	$lastName = "";
+	$email = "";
+	$univerity = "";
+
+    //Connect to mySQL
+	$conn = new mysqli("localhost", "AdminUser", "cop4710Data@", "EventPlanner");
+    if ($conn->connect_error)
+    {
+        returnWithError($conn->connect_error);
+    }
+    else
+	{
+		//Search for the clients userName and password then select data from database
+		$stmt = $conn->prepare("SELECT userID,firstName,lastName,email,university FROM Users WHERE userName=? AND password=?");
+		$stmt->bind_param("ss", $inData["userName"], $inData["password"]);
+		$stmt->execute();
+		$result = $stmt->get_result();	
+
+		//Check if the correct userName & password matches a registered user
+		if( $row = $result->fetch_assoc()  )
+		{
+			//Return the JSON data
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['userID'], $row['email'], $row['university'] );
+		}
+		else
+		{
+			returnWithError("No Records Found");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+	
+	//Get JSON from client, return as object
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	//Send JSON to client
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	//Return JSON to user with an error message when a bad request occurs or unable to find a user.
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	//Return JSON to user with the correct users info
+	function returnWithInfo( $firstName, $lastName, $userID, $email, $univerity )
+	{
+		$retValue = '{"id":' . $userID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","email":"' . $email . '", "univerity":"' . $univerity . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>

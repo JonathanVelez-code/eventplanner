cf1a499b72439ff7efa29a042446c91ccf06bb18
Tiffany Petrovic <petrovic.tiffany@gmail.com>
2023-03-28 11:22:42 -0400

    sign in, error incorrect user/pass, sign up link

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 // Const Url and extention to reference differnet endpoints in the same JS file
 const urlBase = 'http://eventsplanneruni.com/';
-const extension = 'html';
+const extension = 'php';
 
 // Init the userId and first and last name to prevent issues
 let userId = 0;
@@ -33,17 +33,20 @@ function doLogin()
 	let jsonPayload = JSON.stringify( tmp );
 	// This just assembles teh URL to allow this JS file to be used with differnet
 	// API endpoints.
-	let url = urlBase + '/index.' + extension;
+	let url = urlBase + 'LAMPAPI/login.' + extension;
 
 	let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
+        //window.location.href = "index.html";
 		xhr.onreadystatechange = function()
 		{
+            
 			if (this.readyState == 4 && this.status == 200)
 			{
+                
 				let jsonObject = JSON.parse( xhr.responseText );
 				// userId is the key that corresponds to each of our registered users
 				userId = jsonObject.id;
@@ -56,8 +59,8 @@ function doLogin()
 				}
 
 				// breaks down the JSON we recieved and sets them to our variables.
-				firstName = jsonObject.firstName;
-				lastName = jsonObject.lastName;
+				username = jsonObject.username;
+				password = jsonObject.password;
 
 				saveCookie();
 				// take us to the landing page! We are Logged in!

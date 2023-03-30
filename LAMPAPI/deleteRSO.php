c135581ad650bdd7409c38726c9ff2e876b03c28
Jonathan Velez <55106987+JonathanVelez-code@users.noreply.github.com>
2023-03-30 16:50:14 -0400

    rso update

--- LAMPAPI/deleteRSO.php
+++ LAMPAPI/deleteRSO.php
@@ -3,8 +3,7 @@
 $inData = getRequestInfo();
 
 //Get fields from clients JSON POST
-$rsoID = $inData['rsoID'];
-$adminID = $inData['adminID'];
+$userID = $inData['userID'];
 $name = $inData['name'];
 
 //Connect to mySQL
@@ -14,21 +13,29 @@ $conn = new mysqli('localhost', 'AdminUser', 'cop4710Data@', 'EventPlanner');
 if ($conn->connect_error) {
     returnWithError(-10, $conn->connect_error);
 } else {
-    $stmt = $conn->prepare('DELETE FROM RSO WHERE rsoID=?');
-    $stmt->bind_param('i', $rsoID);
+    // Get the new RSO ID
+    $stmt = $conn->prepare('SELECT rsoID FROM RSO WHERE name=?');
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_assoc();
+    $rsoid = $row['rsoID'];
+
+    $stmt = $conn->prepare('DELETE FROM Members WHERE userID=? AND rsoID=?');
+    $stmt->bind_param('ii', $userID, $rsoid);
     $stmt->execute();
 
-    $stmt = $conn->prepare('SELECT rsoID FROM RSO WHERE adminID=?');
-    $stmt->bind_param('i', $adminID);
+    $stmt = $conn->prepare('SELECT MembersID FROM Members WHERE rsoID=?');
+    $stmt->bind_param('i', $rsoid);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows < 5) {
-        $numChange = 1; //initialize the $numChange variable
-        $stmt = $conn->prepare('UPDATE RSO SET activation = ? WHERE name= ?');
-        $stmt->bind_param('is', $numChange, $name);
+        $numChange = 1; // Initialize the $numChange variable
+        $stmt = $conn->prepare('UPDATE RSO SET activation = ? WHERE rsoID= ?');
+        $stmt->bind_param('ii', $numChange, $rsoid);
         $stmt->execute();
-        print 'Not active';
+        echo 'Not active';
     }
 
     $stmt->close();
---
+++ LAMPAPI/joinRso.php
@@ -0,0 +1,74 @@
+<?php
+
+$inData = getRequestInfo();
+
+//Get fields from clients JSON POST
+$userID = $inData['userID'];
+$name = $inData['name'];
+
+//Connect to mySQL
+$conn = new mysqli('localhost', 'AdminUser', 'cop4710Data@', 'EventPlanner');
+
+//test if there any connection errors
+if ($conn->connect_error) {
+    returnWithError(-10, $conn->connect_error);
+} else {
+    //Find if the rso exists or not
+    $stmt = $conn->prepare('SELECT rsoID FROM RSO WHERE name=?');
+    $stmt->bind_param('s', $name);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    //Check if name exists
+    if ($row = $result->fetch_assoc()) {
+        $rsoID = $row['rsoID'];
+        $stmt = $conn->prepare(
+            'INSERT INTO Members (userID, rsoID, role) VALUES (?,?, 0)'
+        );
+        $stmt->bind_param('ii', $userID, $rsoID);
+        $stmt->execute();
+
+        $stmt = $conn->prepare('SELECT MembersID FROM Members WHERE rsoID=?');
+        $stmt->bind_param('i', $rsoID);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows >= 5) {
+            $numChange = 0; //initialize the $numChange variable
+            $stmt = $conn->prepare(
+                'UPDATE RSO SET activation = ? WHERE rsoID= ?'
+            );
+            $stmt->bind_param('ii', $numChange, $rsoID);
+            $stmt->execute();
+            print 'active';
+        }
+    } else {
+        //RSO name does not exist
+        returnWithError(0, 'RSO name does not exist');
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
+//Get JSON from client, return as object
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+//Send JASON to client
+//PARAM: $obj - A JSON object
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($errID, $errSTR)
+{
+    $retValue = '{"id":"' . $errID . '","error":"' . $errSTR . '"}';
+    sendResultInfoAsJson($retValue);
+}
+
+?>
